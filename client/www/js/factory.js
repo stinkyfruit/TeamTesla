e17b5774dbc7706fa1a34dc424a70df95a4e9651
Song Gao <song.gao.103@gmail.com>
2016-01-08 15:38:18 -0600

    Integrate back-end server and database with front-end client.

--- client/www/js/factory.js
+++ client/www/js/factory.js
@@ -5,6 +5,8 @@ angular.module('which.factory', [])
 
 .factory('WhichFactory', function($http) {
 
+  var serverURI = 'http://localhost:5007';
+
   /*
    * choose function is called after a decision has been made.
    * Sends an HTTP POST request to /api/which/{{id}}/judge.
@@ -12,17 +14,17 @@ angular.module('which.factory', [])
   **/
   var choose = function(choice, id, username) {
     //choice === a || b
-    var uri = '/api/which/' + id + '/judge';
-    return {
-      a: 1,
-      b: 1
-    }
-    // return $http.post(uri, {username: username, choice: choice})
-    // .then(function (res) {
-    //   return res.data;
-    // }, function(err) {
-    //   return err;
-    // });
+    var uri = serverURI + '/api/which/' + id + '/judge';
+    // return {
+    //   a: 1,
+    //   b: 1
+    // }
+    return $http.post(uri, {username: username, choice: choice})
+    .then(function (res) {
+      return res.data;
+    }, function(err) {
+      return err;
+    });
   }
 
   /*
@@ -30,18 +32,12 @@ angular.module('which.factory', [])
    * Sends an HTTP GET request to /api/which
   **/
   var getNew = function() {
-    return {
-      id: Math.floor(Math.random() * 100),
-      question: " whatever",
-      thingA: 'http://weknowyourdreams.com/images/chocolate/chocolate-01.jpg',
-      thingB: 'http://cdn2.rosannadavisonnutrition.com/wp-content/uploads/2015/10/chocolate-chocolate-30471811-1024-768.jpg'
-    }
-    // return $http.get('/api/which')
-    //  .then(function(res) {
-    //    return res.data;
-    //  }, function(err) {
-    //    return err;
-    //  });
+    return $http.get(serverURI + '/api/which')
+     .then(function(res) {
+       return res.data;
+     }, function(err) {
+       return err;
+     });
   }
 
   /*
@@ -50,12 +46,12 @@ angular.module('which.factory', [])
   **/
   var submit = function(which) {
     console.log(which);
-     //return $http.post('/api/which', which)
-     //  .then(function(res) {
-     //    return res.data;
-     //  }, function(err) {
-     //    return err;
-     //  });
+     return $http.post(serverURI + '/api/which', which)
+      .then(function(res) {
+        return res.data;
+      }, function(err) {
+        return err;
+      });
   }
   return {
     choose: choose,
