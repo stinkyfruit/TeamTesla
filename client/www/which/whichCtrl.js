efa737e6610ef8a0d73f7489b46583a9839f1aac
Allan Trinh <daigortrinh@gmail.com>
2016-01-17 10:07:24 -0800

    fixed bug with hashkey undefined

--- client/www/which/whichCtrl.js
+++ client/www/which/whichCtrl.js
@@ -19,20 +19,20 @@ angular.module('which.controllers.which', ['which.factory', 'ionic.swoosh.cards'
   $scope.data = {
     username: window.localStorage.getItem('which.userToken'),
     activeSlide: 1,
-    which: [{
+    which: {
       id: $stateParams.id,
       question: $stateParams.question,
       thingA: $stateParams.thingA,
       thingB: $stateParams.thingB,
       imageURI: $stateParams.imageURI
-    }],
+    },
   
   };
 
 
   //This gets called when the user swipes, making a decision with the choice from the user
   $scope.decide = function(result) {
-    WhichFactory.choose(result, $scope.data.which[0].id, $scope.data.username).then(function(votingResult) {
+    WhichFactory.choose(result, $scope.data.which.id, $scope.data.username).then(function(votingResult) {
 
       //Allows for state change, showing new view, second argument is the params being sent in to display results
       $state.go('app.result', {
@@ -46,9 +46,6 @@ angular.module('which.controllers.which', ['which.factory', 'ionic.swoosh.cards'
 
   $scope.$on('discard', function(event, element, card) {
     $scope.decide(card);
-    $scope.data = {
-      
-    };
   });
   $scope.originalData = angular.copy($scope.data);
 
